ab2e4860c6c5833fd9847dde39e21bd30702cda0
Raji777 <36372373+Raji777@users.noreply.github.com>
2018-02-23 17:58:28 +0530

    char.cpp

---
+++ char.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+char ch;
+cout<<"enter charecter";
+cin>>ch;
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+{
+cout<<" the alphabet is vowel";}
+else
+{
+cout<<"the alphabet is consonant";
+}
+return 0;
+}
