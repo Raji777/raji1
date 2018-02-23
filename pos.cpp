944cd16f75b6a63988122ccfa2a0903fd9d28c18
Raji777 <36372373+Raji777@users.noreply.github.com>
2018-02-23 17:20:08 +0530

    pos.cpp

---
+++ pos.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<<"enter n";
+cin>>n;
+if(n>0)
+{cout<<"the no is positive";}
+else if(n<0)
+{cout<<"the no is negative";}
+else
+{cout<<"the no is zero";}
+return 0;
+}
