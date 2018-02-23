72ad2d6e6684d3fde3642c36bbd039043bdd1860
Raji777 <36372373+Raji777@users.noreply.github.com>
2018-02-23 17:29:02 +0530

    even.cpp

---
+++ even.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<<"enter number";
+cin>>n;
+if(n%2==0)
+{
+cout<<"the no is even";
+}
+else
+{
+cout<<"the no is odd";
+}
+return 0;
+}
