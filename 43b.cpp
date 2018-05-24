357bd802753af7eef1d4f0bf409cc64e22c3fcaf
Raji777 <36372373+Raji777@users.noreply.github.com>
2018-05-24 12:15:23 +0530

    43b.cpp

---
+++ 43b.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{   string s1,s2,s;
+    cout<<"enter two strings"<<endl;
+    cin>>s1>>s2;
+    s=s1 + s2;
+    cout<<s;
+    return 0;
+}
