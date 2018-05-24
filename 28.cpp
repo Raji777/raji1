713b035d26ab4b44ddf7e11935f6bc3f61bc7e6c
Raji777 <36372373+Raji777@users.noreply.github.com>
2018-05-24 14:42:47 +0530

    28b.cpp

--- 28.cpp
+++ 28.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 using namespace std;
-int main() 
+int main()
 {
-	int i;
-	int a[5]={1,2,5,4,5};
-	for(i=0;i<5;i++)
-	{   
-		cout<<a[i]<<"\t"<<i<<"\n";
-	}
+int i,a[20],n;
+cin>>n;
+for(i=0;i<=n;i++)
+{
+	cin>>a[i];
+}
+for(i=0;i<=n;i++)
+{
+	cout<<a[i]<<'\t'<<i<<endl;
+}
 	return 0;
 }
