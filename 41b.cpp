22d4549e2bfb3c1e10df7e5d8083c0374af28749
Raji777 <36372373+Raji777@users.noreply.github.com>
2018-05-24 11:56:30 +0530

    41b.cpp

---
+++ 41b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char s[100];
+	int k,i;
+	cout<<"enter the string"<<endl;
+	cin>>s;
+	cout<<"enter k"<<endl;
+	cin>>k;
+	for(i=0;i<k;i++)
+	{
+		cout<<s<<endl;
+	}
+	return 0;
+}
