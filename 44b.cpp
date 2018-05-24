87af3d33d3670e4d968f8079f07b73202dd1910d
Raji777 <36372373+Raji777@users.noreply.github.com>
2018-05-24 12:20:48 +0530

    44b.cpp

---
+++ 44b.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{   
+	int n;
+	cout<<"enter n"<<endl;
+	cin>>n;
+	if(n>=1&&n<=10)
+	{
+		cout<<"yes";
+		
+	}
+	else
+	{
+		cout<<"no";
+	}
+    return 0;
+}
