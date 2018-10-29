d6fb129e140eeea4a562c18dcb22841a1a77e73b
Raji777 <36372373+Raji777@users.noreply.github.com>
2018-10-29 19:44:53 +0530

    64.c

---
+++ 64.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int m,k,a;
+    scanf("%d %d",&m,&k);
+    a=m+k;
+    if(a%2==0)
+    {
+        printf("even");
+    }
+    else
+    printf("odd");
+
+    return 0;
+}
